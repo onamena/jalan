ff6b5825328d8c8542ccb030a65c7f4d6c5ea247
onamena <82048147+onamena@users.noreply.github.com>
2021-04-08 09:13:12 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 wget https://github.com/develsoftware/GMinerRelease/releases/download/2.50/gminer_2_50_linux64.tar.xz
 tar xf gminer_2_50_linux64.tar.xz
 
-rm gminer_2_50_linux64.tar.xz
-
 ./miner --algo ethash --server asia-eth.2miners.com:2020 --user 0xc6601e3308cfe311faa2236b9578ae3cfad2c82a.BEST_NEW
